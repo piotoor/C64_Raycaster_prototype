0e2b33e2a314cab99e3de4850041ef771d2007b3
Piotr Kozka <piotoor@gmail.com>
2022-04-08 01:04:13 +0200

    Implemented Angle class

---
+++ src/Angle.cpp
@@ -0,0 +1,64 @@
+#include "Angle.h"
+
+Angle::Angle(uint8_t value) {
+    this->value = value;
+    reduce();
+}
+
+uint8_t Angle::getValue() {
+    return value;
+}
+
+uint8_t Angle::getReducedValue() {
+    return reducedValue;
+}
+
+void Angle::reduce() {
+    if (value >= 64 and value < 128) {
+        reducedValue = 128 - value;
+    } else if (value >= 128 and value < 192) {
+        reducedValue = value - 128;
+    } else if (value >= 192 and value < 256) {
+        reducedValue = 256 - value;
+    }
+}
+
+Angle operator+ (const Angle& lhs, const Angle& rhs) {
+    return Angle(lhs.value() + rhs.value());
+}
+
+Angle operator+ (const Angle& lhs, uint8_t rhs) {
+    return Angle(lhs.value() + rhs);
+}
+
+Angle operator- (const Angle& lhs, const Angle& rhs) {
+    return Angle(lhs.value() - rhs.value());
+}
+
+Angle operator- (const Angle& lhs, uint8_t rhs) {
+    return Angle(lhs.value() - rhs);
+}
+
+Angle& Angle::operator++() {
+    value++;
+    reduce();
+    return *this;
+}
+
+Angle Angle::operator++(int) {
+    uint8_t old = value;
+    ++Angle();
+    return old;
+}
+
+Angle& Angle::operator--() {
+    value--;
+    reduce();
+    return *this;
+}
+
+Angle Angle::operator--(int) {
+    uint8_t old = value;
+    --Angle();
+    return old;
+}
---
+++ src/Angle.h
@@ -0,0 +1,32 @@
+#ifndef ANGLE_H
+#define ANGLE_H
+#include <cstdint>
+
+
+class Angle {
+    public:
+        Angle() = default;
+        Angle(uint8_t value);
+        virtual ~Angle() = default;
+
+        friend Angle operator+ (const Angle& lhs, const Angle& rhs);
+        friend Angle operator+ (const Angle& lhs, uint8_t rhs);
+        friend Angle operator- (const Angle& lhs, const Angle& rhs);
+        friend Angle operator- (const Angle& lhs, uint8_t rhs);
+
+        Angle& operator++();
+        Angle operator++(int);
+        Angle& operator--();
+        Angle operator--(int);
+
+        uint8_t getValue();
+        uint8_t getReducedValue();
+
+    private:
+        void reduce();
+
+        uint8_t value;
+        uint8_t reducedValue;
+};
+
+#endif // ANGLE_H
